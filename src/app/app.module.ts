8a58ca53724ed1276538cd2293396392f745ac01
snake324 <kristi4n90@gmail.com>
2023-06-19 11:15:37 +0200

    components-created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './components/form/form.component';
+import { ListComponent } from './components/list/list.component';
+import { FeaturesComponent } from './components/list/features/features.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    ListComponent,
+    FeaturesComponent
   ],
   imports: [
     BrowserModule,
